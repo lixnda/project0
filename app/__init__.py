9a31d0640e9a2cb7a62d42b565cb13b3aa00cd7e
Mark Ma <markworldamc@gmail.com>
2024-10-29 10:56:32 -0400

    a

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,41 @@ from flask import request
 import sqlite3, csv
 
 app = Flask(__name__)
+# add other functions if needed
 
 @app.route("/")
-def homepage():
+def home():
     return "hi"
-    
+
+@app.route("/profile")
+def profile():
+    return "hi"
+
+# optional search feature at /search
+
+@app.route("/login")
+def login():
+    return "hi"
+
+@app.route("/logout")
+def logout():
+    return "hi"
+
+# /follow/user ID to follow user
+@app.route("/follow")
+    return "hi"
+
+# create blog here
+@app.route("/create")
+def create():
+    return "hi"
+
+# for blogs you can make /blogs/blog ID   
+# for blog editing you can make /blogs/blog ID/edit
+
+def blogs():
+    return "hi"
+
 if __name__ == "__main__":
     app.debug = True 
     app.run()
