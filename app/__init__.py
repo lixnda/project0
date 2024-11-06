4c81518e719f6a99342b9a3edd495e6bce7d7fd9
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2024-11-06 11:01:31 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -150,19 +150,22 @@ def create():
 # for blogs you can make /blogs/blog ID
 # for blog editing you can make /blogs/blog ID/edit
 
-def display_blogs():
-    idx = 0
-    text = ""
-    user = ""
-    title = ""
-    date = ""
-    for row in result:
-        pull = row  #gets the latest row of info b/c it will loop and change pull until it gets to the end
-    user = pull[0]
-    idx = pull[1]
-    date = pull[2]
-    title = pull[3]
-    content = pull[4]
+def display_blogs(blog_id):
+    rows = [
+        (1, 123, "This is a Title", "These are the contents ", 1, "Bob"),
+        (2, 321, "This is a Title For Blog 2",
+         "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes iaculis auctor magnis sagittis maecenas egestas class velit. Hac odio erat tellus penatibus, nunc dis litora. Odio egestas est dignissim sodales nec tempor parturient massa. Class ultricies torquent himenaeos sit libero dignissim libero. Vel facilisi mollis morbi ad magna cursus sollicitudin fringilla. Vel pharetra interdum at varius integer habitasse. Molestie curabitur euismod in viverra blandit sociosqu id. Litora aptent volutpat posuere porttitor fringilla.",
+         2, "Joe")
+    ]
+    idx = blog_id
+    user = rows[0][5]
+    date = datetime.utcfromtimestamp(rows[0][1]).strftime('%Y-%m-%d %H:%M:%S')
+    title = rows[2]
+    text = rows[3]
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
 
 if __name__ == "__main__":
     app.debug = True
